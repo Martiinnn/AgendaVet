0334d326ae6b061bb53f1b15682f48cfa12a600e
Martinn__ <75870685+Martiinnn@users.noreply.github.com>
2025-06-23 09:30:04 -0400

    fix

--- AgendaVet/src/main/java/com/AgendaVet/AgendaVet/config/SwaggerConfig.java
+++ AgendaVet/src/main/java/com/AgendaVet/AgendaVet/config/SwaggerConfig.java
@@ -2,12 +2,8 @@ package com.AgendaVet.AgendaVet.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
 @Configuration
 public class SwaggerConfig {
 
